a201a5696c2251344fd75f1d241b6d83f49a874a
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-20 11:23:16 +0700

    completing all files

--- API_ORM/index.js
+++ API_ORM/index.js
@@ -58,3 +58,15 @@ app.put('/komik/:id', async (req, res) => {
 
 app.delete('/komik/:id', async (req, res) => {
     const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
+        await komik.destroy();
+        res.send({ message: 'Komik deleted successfully' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
