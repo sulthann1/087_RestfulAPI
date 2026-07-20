4ced73ac1b05dcb583435c9f81bb767ac6c6108a
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-20 11:11:20 +0700

    menambahkan post, put, get di index.js

--- API_ORM/index.js
+++ API_ORM/index.js
@@ -20,3 +20,26 @@ app.use(express.urlencoded({
 .catch((err) => {
     console.log(err);
 })
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    
